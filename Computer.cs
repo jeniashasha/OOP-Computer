5f56c15423f09b90ad87368d63578bf293437995
jeniashasha <66595992+jeniashasha@users.noreply.github.com>
2020-06-17 19:28:06 +0300

    Add files via upload

---
+++ Computer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+  
+    class Computer
+    {
+        public string model;
+        public int price;
+        public int numberOfProcessors;
+        public float screenSize;
+        public bool isTurnOn = true;
+
+        public int TellMeThePrice()
+        {
+            Console.WriteLine($"the price is {price}");
+            return price;
+        }
+        public float TellMeTheScreenSize()
+        {
+            Console.WriteLine($"the screen size is {screenSize}");
+            return screenSize;
+        }
+        public void TurnOn()
+        {
+            if (isTurnOn)
+            {
+                Console.WriteLine($"{model} is already on!");
+            }
+            else
+            {
+                isTurnOn = false;
+                Console.WriteLine($"turning on {model}");
+            }
+        }
+        public void TurnOff()
+        {
+            if (!isTurnOn)
+            {
+                Console.WriteLine($"{model} is turning off");
+            }
+        }
+        public void AddProcessor(int processors)
+        {
+            numberOfProcessors = numberOfProcessors + processors;
+            Console.WriteLine($"{model} has got {processors} new processors and now has got {numberOfProcessors}");
+        }
+
+        public override string ToString()
+        {
+            return $"Class Computer model = {model} number of processors = {numberOfProcessors} screen size = {screenSize}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer myLenovo = new Computer();
+            {
+                myLenovo.model = "ThinkPad";
+                myLenovo.price = 2500;
+                myLenovo.numberOfProcessors = 2;
+                myLenovo.screenSize = 14.5f;
+                myLenovo.TellMeThePrice();
+                myLenovo.TellMeTheScreenSize();
+                myLenovo.TurnOn();
+                myLenovo.TurnOff();
+                myLenovo.AddProcessor(4);
+
+            }
+
+
+            Computer myAsus = new Computer()
+            {
+                model = "ZenBook",
+                price = 4750,
+                numberOfProcessors = 4,
+                screenSize = 14.2f,
+            isTurnOn = true
+            };
+        Console.WriteLine(myAsus);
+
+            Console.WriteLine(myAsus.TellMeTheScreenSize());
+            
+            myAsus.TurnOn();
+            myAsus.AddProcessor(8);
+        
+            
+
+            Computer myHp = new Computer();
+            {
+                myHp.model = "EliteBook";
+                myHp.price = 3800;
+                myHp.numberOfProcessors = 8;
+                myHp.screenSize = 14.6f;
+                myHp.TellMeThePrice();
+                myHp.TellMeTheScreenSize();
+                myHp.TurnOn();
+                myHp.TurnOff();
+                myHp.AddProcessor(2);
+            }
+            Console.WriteLine(myHp);
+        }
+    }
+}
